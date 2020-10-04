1b37a371085910f3dc3983a6b1e4445c32c14f75
anokhi shah <anokhias@gmail.com>
2020-10-04 20:05:44 +0200

    WSC-19: refactoring mqtt client, separate interface and implementation

--- sensor_interface/headers/mqtt_client.hpp
+++ sensor_interface/headers/mqtt_client.hpp
@@ -18,28 +18,31 @@ namespace WS{
             MQTTClient(const std::string& ipAddress, const std::string& clientId);
             void Connect();
             bool IsConnected() const noexcept;
-            
             template<typename Topic, typename Payload>
-            Result PublishMessage(Topic&& topic, Payload&& payload){
-                Result retval = Result::Successful;
-                try{
-                    mqtt::message_ptr pubmsg = mqtt::make_message(std::forward<Topic>(topic), 
-                                                        std::forward<Payload>(payload));
-                    pubmsg->set_qos(1);
-                    auto pubtok = client_->publish(pubmsg)->wait_for(500ms);
-
-                }
-                catch(std::exception& e){
-                    std::cerr << "MQTTClient::Publish threw: " << e.what() << "\n";
-                    retval = Result::Unsuccessful;   
-                }
-
-                return retval;
-            }
+            Result PublishMessage(Topic&& topic, Payload&& payload);
 
         private:
             std::unique_ptr<mqtt::async_client> client_;
     };
 
+//Implementation of perfect forwarding PublishMessage
+template<typename Topic, typename Payload>
+Result MQTTClient::PublishMessage(Topic&& topic, Payload&& payload)
+{
+    Result retval = Result::Successful;
+    try{
+        mqtt::message_ptr pubmsg = mqtt::make_message(std::forward<Topic>(topic), 
+                                            std::forward<Payload>(payload));
+        pubmsg->set_qos(1);
+        auto pubtok = client_->publish(pubmsg)->wait_for(500ms);
+
+    }
+    catch(std::exception& e){
+        std::cerr << "MQTTClient::Publish threw: " << e.what() << "\n";
+        retval = Result::Unsuccessful;   
+    }
+
+    return retval;
+}
 } //namespace WS
 #endif //MQTT_CLIENT_H

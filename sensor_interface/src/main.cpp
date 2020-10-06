e96f2ff3047a0b294b8516f1f72108ef7479e04e
gajendra gulgulia <gajendra.gulgulia@gmail.com>
2020-10-06 21:23:31 +0200

    WSC-24: cyclic publish

--- sensor_interface/src/main.cpp
+++ sensor_interface/src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "TemperaturePublisher.hpp"
 
+using namespace WS;
+
 int main(){
     std::cout << "Hello Weather Station\n";
+    std::shared_ptr<MQTTClient> client = std::make_shared<MQTTClient>("localhost", "main");
+    TemperaturePublisher temp_publisher{client};
+    std::string input;
+    while (std::getline(std::cin, input) && input != "q"){
+        temp_publisher.publish();
+    }
     return 0;
 }

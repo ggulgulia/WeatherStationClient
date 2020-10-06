5afcd7096dc5665212930811898dd4baa0321a3c
gajendra gulgulia <gajendra.gulgulia@gmail.com>
2020-10-06 21:31:03 +0200

    WSC-25: Testing cyclic publishing

--- sensor_interface/src/main.cpp
+++ sensor_interface/src/main.cpp
@@ -8,7 +8,7 @@ int main(){
     std::shared_ptr<MQTTClient> client = std::make_shared<MQTTClient>("localhost", "main");
     TemperaturePublisher temp_publisher{client};
     std::string input;
-    while (std::getline(std::cin, input) && input != "q"){
+    while (1){
         temp_publisher.publish();
     }
     return 0;

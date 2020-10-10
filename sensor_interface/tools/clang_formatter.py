3ecff651e7e0a987226bc2a16dd60470514dc214
gajendra gulgulia <gajendra.gulgulia@gmail.com>
2020-10-10 20:28:48 +0200

    WSC-02: clang format helper script

---
+++ sensor_interface/tools/clang_formatter.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+from pathlib import Path
+
+def apply_clang_formatter():
+    #bin_path  = 'bin/output'
+    file_list = []      # The list of .cpp files to be compiled
+
+    for a_file in Path('.').glob('**/*.cpp'):
+        file_list.append(str(a_file))
+
+    for a_file in Path('.').glob('**/*.hpp'):
+        file_list.append(str(a_file))
+        
+    cpp_file_sep = ' '
+    command = 'clang-format -i ' + cpp_file_sep.join(file_list)
+    print(subprocess.getoutput(command))
+
+
+if __name__ == '__main__':
+    apply_clang_formatter()
